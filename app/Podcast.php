e23a58da1c0ca5d275a5719f6ca00c1627b38761
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-04 21:36:16 +0800

    finish podcast model

--- app/Podcast.php
+++ app/Podcast.php
@@ -8,11 +8,23 @@ use Illuminate\Support\Facades\Storage;
 class Podcast extends Model
 {
     protected $guarded = [];
-    protected $casts = [ 'published_at' => 'datetime' ];
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
 
     public function scopePublished($query)
     {
         return $query->whereNotNull('published_at');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'subscriptions')->withTimestamps();
+    }
+
+    public function subscriptions()
+    {
+        return $this->hasMany(Subscription::class);
     }
 
     public function episodes()
@@ -20,9 +32,29 @@ class Podcast extends Model
         return $this->hasMany(Episode::class);
     }
 
+    public function publish()
+    {
+        $this->update(['published_at' => $this->freshTimestamp()]);
+    }
+
+    public function unpublish()
+    {
+        $this->update(['published_at' => null]);
+    }
+
     public function recentEpisodes($count = 5)
     {
         return $this->episodes()->recent()->take($count)->get();
+    }
+
+    public function imageUrl()
+    {
+        return Storage::disk('public')->url($this->cover_path);
+    }
+
+    public function websiteHost()
+    {
+        return parse_url($this->website, PHP_URL_HOST);
     }
 
     public function isVisibleTo($user)
@@ -38,5 +70,21 @@ class Podcast extends Model
     public function isOwnedBy($user)
     {
         return $this->user_id == $user->getKey();
+    }
+
+    public function toArray()
+    {
+        return array_merge(parent::toArray(), [
+            'cover_url' => $this->imageUrl(),
+            'published' => $this->isPublished(),
+            'subscribed' => auth()->user()->subscribesTo($this),
+            'links' => [
+                'publish' => url("/podcasts/{$this->id}/publish"),
+                'unpublish' => url("/podcasts/{$this->id}/unpublish"),
+                'subscribe' => url("/podcasts/{$this->id}/subscribe"),
+                'unsubscribe' => url("/podcasts/{$this->id}/unsubscribe"),
+                'update_cover_image' => url("/podcasts/{$this->id}/cover-image"),
+            ],
+        ]);
     }
 }

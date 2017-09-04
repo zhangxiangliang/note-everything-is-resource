7ae5bbab3128ebf5a269e254733b1aeb13c08058
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-04 21:37:11 +0800

    finish user model

--- app/User.php
+++ app/User.php
@@ -16,4 +16,24 @@ class User extends Authenticatable
     {
         return $this->hasMany(Podcast::class);
     }
+
+    public function subscribedPodcasts()
+    {
+        return $this->belongsToMany(Podcast::class, 'subscriptions')->withTimestamps();
+    }
+
+    public function subscriptions()
+    {
+        return $this->hasMany(Subscription::class);
+    }
+
+    public function subscribesTo($podcast)
+    {
+        return $this->subscribedPodcasts()->where($podcast->getQualifiedKeyName(), $podcast->getKey())->count() > 0;
+    }
+
+    public function subscriptionTo($podcast)
+    {
+        return $this->subscriptions()->where('podcast_id', $podcast->id)->first();
+    }
 }

ad2fe96b6cac07da539d402fdc79befc06444c55
ZHANGXIANGLIANG <326277403@qq.com>
2017-09-04 21:00:46 +0800

    create routes web

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+Route::get('/podcasts', 'PodcastsController@index');
+Route::get('/podcasts/new', 'PodcastsController@create');
+Route::post('/podcasts', 'PodcastsController@store');
+Route::get('/podcasts/{id}', 'PodcastsController@show');
+Route::get('/podcasts/{id}/edit', 'PodcastsController@edit');
+Route::patch('/podcasts/{id}', 'PodcastsController@update');
+Route::delete('/podcasts/{id}', 'PodcastsController@destroy');
+
+Route::get('/episodes', 'EpisodesController@index');
+Route::get('/episodes/{id}', 'EpisodesController@show');
+Route::get('/episodes/{id}/edit', 'EpisodesController@edit');
+Route::patch('/episodes/{id}', 'EpisodesController@update');
+
+Route::get('/podcasts/{id}/episodes', 'PodcastEpisodesController@index');
+Route::post('/podcasts/{id}/episodes', 'PodcastEpisodesController@store');
+Route::get('/podcasts/{id}/episodes/new', 'PodcastEpisodesController@create');
